be7f846d471f6360ac7743598a0753e6fcdfe045
Mathis_D <mathis.d@acistec.fr>
2025-06-01 11:53:10 +0200

    added licencing and copyright notice

--- src/utils/sample_buffer.h
+++ src/utils/sample_buffer.h
@@ -1,3 +1,15 @@
+// This file is part of the Open Audio Live System project, a live audio environment
+// Copyright (c) 2025 - Mathis DELGADO
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
 #ifndef OPENDSP_SAMPLE_BUFFER_H
 #define OPENDSP_SAMPLE_BUFFER_H
 
