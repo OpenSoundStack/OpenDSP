5a83b1395ed5838e5c6dc97394cc004b7d3b9111
Mathis_D <mathis.d@acistec.fr>
2026-04-04 17:05:43 +0200

    main license update

--- src/utils/values.cpp
+++ src/utils/values.cpp
@@ -1,14 +1,7 @@
 // This file is part of the Open Audio Live System project, a live audio environment
-// Copyright (c) 2025 - Mathis DELGADO
+// Copyright (c) 2026 - Mathis DELGADO
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, version 3 of the License.
-//
-// This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+// This project is distributed under the Creative Commons CC-BY-NC-SA licence. https://creativecommons.org/licenses/by-nc-sa/4.0
 
 #include "values.h"
 
